3ebc31b6050cf71abe5962eb357fe5a44ca06a3e
pawankumarbawa <137701904+pawankumarbawa@users.noreply.github.com>
2023-06-25 09:46:18 -0400

    added sys.out

--- Solutions/src/com/test/basics/Calculator.java
+++ Solutions/src/com/test/basics/Calculator.java
@@ -69,6 +69,7 @@ public class Calculator {
         System.out.println(calculator.subtract(5.5, 2.3));
         System.out.println(calculator.subtract(10.7, 4.2, 2.1));
         System.out.println(calculator.subtract(20.5, 5.2, 3.1, 2.4));
+	 System.out.println("pawan:");
     }
 
 	public double multiply(int num1, int num2) {
