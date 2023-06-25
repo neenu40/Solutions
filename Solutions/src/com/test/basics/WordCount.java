65488a5a37b00f06b6e8efc918e2569b661e38c9
Gayathiridevi <pgayathiridevi94@gmail.com>
2023-06-25 12:02:35 -0400

    testing branch stmt added

--- Solutions/src/com/test/basics/WordCount.java
+++ Solutions/src/com/test/basics/WordCount.java
@@ -10,7 +10,7 @@ public class WordCount {
         String sentence = scanner.nextLine();
 
         int wordCount = countWords(sentence);
-       
+       System.out.println("Branch Testing check");
 		System.out.println("Total number of words Recent Update Gaya: " + wordCount);
     }
 
